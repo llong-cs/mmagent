9bc600e03442aabd1577907e20c5827945752d94
llong-cs <kylin0long@gmail.com>
2025-05-04 21:47:45 +0800

    update evaluate sh

--- evaluation/evaluate.sh
+++ evaluation/evaluate.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
-ckpt_path=${1:-"/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-600"}
-val_path=${2:-"data/sft/memgen/0429/conversations/val.jsonl"}
-output_dir=${3:-"data/sft/memgen/0429/evaluation"}
-node_num=${4:-8}
-val_num=${5:-200}
+# Define variables
+val_path="data/sft/memgen/0429/conversations/val.jsonl"
+output_dir="data/sft/memgen/0429/evaluation"
+node_num=8
+val_num=200
 
-# for i in $(seq 0 $((node_num - 1))); do
-#     CUDA_VISIBLE_DEVICES=$i python3 evaluation/sft_evaluation.py \
-#         --ckpt_path ${ckpt_path} \
-#         --val_path ${val_path} \
-#         --output_dir ${output_dir} \
-#         --generate \
-#         --cuda_id $i & \
-# done
-# wait
+# Define an array of checkpoint paths
+ckpt_paths=(
+    # "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-600"
+    "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-900"
+    "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-1200"
+    "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-1500"
+    "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-1800"
+    "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-2100"
+    "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-2400"
+    "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-2700"
+    "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts/checkpoint-3000"
+)
 
-python3 evaluation/sft_evaluation.py \
-    --ckpt_path ${ckpt_path} \
-    --val_path ${val_path} \
-    --output_dir ${output_dir} \
-    --val_num ${val_num}
+# Loop through each checkpoint path
+for ckpt_path in "${ckpt_paths[@]}"; do
+    # for i in $(seq 0 $((node_num - 1))); do
+    #     CUDA_VISIBLE_DEVICES=$i python3 evaluation/sft_evaluation.py \
+    #         --ckpt_path ${ckpt_path} \
+    #         --val_path ${val_path} \
+    #         --output_dir ${output_dir} \
+    #         --generate \
+    #         --cuda_id $i & \
+    # done
+    # wait
+
+    python3 evaluation/sft_evaluation.py \
+        --ckpt_path ${ckpt_path} \
+        --val_path ${val_path} \
+        --output_dir ${output_dir} \
+        --val_num ${val_num}
+done
