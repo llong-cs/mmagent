6c8f9d7b457efe92b428585df7da2f7b99dde4a7
llong-cs <kylin0long@gmail.com>
2025-05-04 16:43:24 +0800

    add evaluation sh

---
+++ evaluation/evaluate.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ckpt_path=${1:-"/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts"}
+val_path=${2:-"data/sft/memgen/0429/conversations/val.jsonl"}
+output_dir=${3:-"data/sft/memgen/0429/val_gen"}
+node_num=${4:-8}
+val_num=${5:-10}
+
+for i in $(seq 0 $((node_num - 1))); do
+    CUDA_VISIBLE_DEVICES=$i python3 sft_evaluation.py \
+        --ckpt_path ${ckpt_path} \
+        --val_path ${val_path} \
+        --output_dir ${output_dir} \
+        --generate \
+        --cuda_id $i & \
+done
+wait
+
+python3 sft_evaluation.py \
+    --val_path ${val_path} \
+    --output_dir ${output_dir} \
+    --val_num ${val_num}
--- evaluation/sft_evaluation.py
+++ evaluation/sft_evaluation.py
@@ -1,10 +1,26 @@
 import json
+import os
+import argparse
+from tqdm import tqdm
 
 from transformers import Qwen2_5OmniProcessor, Qwen2_5OmniThinkerForConditionalGeneration, Qwen2_5OmniThinkerConfig, GenerationConfig
-from transformers.utils import ModelOutput
 from qwen_omni_utils import process_mm_info
+from evaluation.memory_evaluation import eval_vdcscore, eval_autodq
+from mmagent.utils.general import validate_and_fix_python_list
 
-def evaluate_sft(model, processor, data_path, val_num=5):
+parser = argparse.ArgumentParser()
+parser.add_argument("--ckpt_path", type=str, default="/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts")
+parser.add_argument("--val_path", type=str, default="data/sft/memgen/0429/conversations/val.jsonl")
+parser.add_argument("--output_dir", type=str, default="data/sft/memgen/0429/val_gen")
+parser.add_argument("--val_num", type=int, default=5)
+parser.add_argument("--cuda_id", type=int, default=0)
+parser.add_argument("--node_num", type=int, default=8)
+parser.add_argument("--debug", action="store_true")
+parser.add_argument("--generate", action="store_true")
+args = parser.parse_args()
+
+
+def evaluate_sft_compare(model, processor, data_path, val_num=5):
     model.eval()
     count = 0
 
@@ -46,9 +62,92 @@ def evaluate_sft(model, processor, data_path, val_num=5):
             if count >= val_num:
                 break
 
+def generate_sft_data(model, processor, data_path, output_dir):
+    model.eval()
+    samples = []
+    node_num = args.node_num
+    cuda_id = args.cuda_id
+
+    with open(data_path, "r") as f:
+        for line in f:
+            sample = json.loads(line)
+            samples.append(sample)
+
+    for i, sample in enumerate(tqdm(samples)):
+        if i % node_num != cuda_id:
+            continue
+        messages = sample[:1]
+        text = processor.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
+        generation_config = GenerationConfig(pad_token_id=151643, bos_token_id=151644, eos_token_id=151645)
+        try:
+            USE_AUDIO_IN_VIDEO = True
+            audios, images, videos = process_mm_info(messages, use_audio_in_video=USE_AUDIO_IN_VIDEO)
+            inputs = processor(text=text, audios=audios, images=images, videos=videos, return_tensors="pt", padding=True, use_audio_in_video=USE_AUDIO_IN_VIDEO)
+            inputs = inputs.to(model.device).to(model.dtype)
+
+            # Inference: Generation of the output text and audio
+            generation = model.generate(**inputs, generation_config=generation_config, use_audio_in_video=USE_AUDIO_IN_VIDEO, max_new_tokens=2048)
+            generate_ids = generation[:, inputs.input_ids.size(1):]
+        except:
+            USE_AUDIO_IN_VIDEO = False
+            audios, images, videos = process_mm_info(messages, use_audio_in_video=USE_AUDIO_IN_VIDEO)
+            inputs = processor(text=text, audios=audios, images=images, videos=videos, return_tensors="pt", padding=True, use_audio_in_video=USE_AUDIO_IN_VIDEO)
+            inputs = inputs.to(model.device).to(model.dtype)
+
+            # Inference: Generation of the output text and audio
+            generation = model.generate(**inputs, generation_config=generation_config, use_audio_in_video=USE_AUDIO_IN_VIDEO, max_new_tokens=2048)
+            generate_ids = generation[:, inputs.input_ids.size(1):]
+
+        response = processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+        messages.append({"role": "assistant", "content": response})
+        
+        with open(os.path.join(output_dir, f"{i}.json"), "w") as f:
+            json.dump(messages, f, indent=4, ensure_ascii=False)
+
+def evaluate_sft(gt_path, output_dir, val_num=10):
+    gt_samples = []
+    pred_samples = []
+    with open(gt_path, "r") as f:
+        for line in f:
+            sample = json.loads(line)
+            gt_samples.append(validate_and_fix_python_list(sample[1]["content"][0]["text"]))
+    
+    pred_files = os.listdir(output_dir)
+    # order by number
+    pred_files = sorted(pred_files, key=lambda x: int(x.split(".")[0]))
+    for pred_file in pred_files:
+        with open(os.path.join(output_dir, pred_file), "r") as f:
+            sample = json.load(f)
+            pred_samples.append(validate_and_fix_python_list(sample[1]["content"][0]["text"]))
+    
+    assert len(gt_samples) == len(pred_samples)
+
+    gt_samples = gt_samples[:val_num]
+    pred_samples = pred_samples[:val_num]
+    
+    print("Evaluating AutoDQ...")
+    precision, recall, f1 = eval_autodq(gt_samples, pred_samples)
+    print("AutoDQ Evaluation:")
+    print(f"Precision: {precision}, Recall: {recall}, F1: {f1}")
+    
+    print("Evaluating VDCScore...")
+    precision, avg_score = eval_vdcscore(gt_samples, pred_samples)
+    print(f"VDCScore Evaluation:")
+    print(f"Precision: {precision}, Avg Score: {avg_score}")
+            
+            
 if __name__ == "__main__":
-    ckpt_path = "/mnt/hdfs/foundation/longlin.kylin/mmagent/data/memgen_sft/0429/ckpts"
-    val_path = "data/sft/memgen/0429/conversations/val.jsonl"
+    ckpt_path = args.ckpt_path
+    val_path = args.val_path
+    output_dir = args.output_dir
+    os.makedirs(output_dir, exist_ok=True)
     model = Qwen2_5OmniThinkerForConditionalGeneration.from_pretrained(ckpt_path, torch_dtype="auto", device_map="auto", attn_implementation="flash_attention_2")
     processor = Qwen2_5OmniProcessor.from_pretrained(ckpt_path)
-    evaluate_sft(model, processor, val_path)
+    if args.debug:
+        evaluate_sft_compare(model, processor, val_path, val_num=args.val_num)
+        exit()
+    if args.generate:
+        generate_sft_data(model, processor, val_path, output_dir)
+    else:
+        evaluate_sft(val_path, output_dir, val_num=args.val_num)
+    
