428ac70e26f9a8cb3e9ab163269164c0807c9f06
llong-cs <kylin0long@gmail.com>
2025-05-07 20:08:55 +0800

    update qa gen

--- data_preparation/generate_qa.py
+++ data_preparation/generate_qa.py
@@ -4,10 +4,10 @@ from mmagent.retrieve import translate
 from mmagent.prompts import prompt_generate_qa_from_route
 from mmagent.utils.chat_api import generate_messages, get_response_with_retry, parallel_get_response
 import json
-import os
 import argparse
 import sys
-
+from concurrent.futures import ProcessPoolExecutor
+from tqdm import tqdm
 sys.modules["videograph"] = mmagent.videograph
 
 processing_config = json.load(open("configs/processing_config.json", "r"))
@@ -45,22 +45,55 @@ def generate_qa_from_route(route, route_contents):
 def generate_qa_from_mem(mem_path, length=10):
     route, route_contents = get_route_from_mem(mem_path, length)
     res = generate_qa_from_route(route, route_contents)
-    return route, route_contents, res
+    return res
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mem_path", type=str, help="The path to the memory file", default="/mnt/hdfs/foundation/longlin.kylin/mmagent/data/mems_qwen/CZ_1/EMpaw46kr2w_30_5_-1_10_20_0.3_0.6.pkl")
     parser.add_argument("--length", type=int, help="The length of the route", default=10)
     args = parser.parse_args()
+    
+    length = args.length
 
-    lengths = [5, 10, 15, 20, 25, 30]
+    mem_paths = []
+    with open("data/annotations/train_500.jsonl", "r") as f:
+        for line in f:
+            data = json.loads(line)
+            if data["mem_path"] not in mem_paths:
+                mem_paths.append(data["mem_path"])
+    mem_paths = mem_paths + mem_paths
+    
+    print(f"number of mems: {len(mem_paths)}")
+    
+    qas_from_mems = []
+    
+    def process_mem(mem_path, length):
+        res = generate_qa_from_mem(mem_path, length)
+        return {
+            "mem_path": mem_path,
+            "question": res["question"],
+            "answer": res["answer"], 
+            "related_ids": res["related_ids"]
+        }
 
-    for length in lengths:
-        print(f"length: {length}")
-        route, route_contents, qa = generate_qa_from_mem(args.mem_path, length)
-        print(qa)
-        print(f"related nodes: {route}")
-        print("-"*20)
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        qas_from_mems = list(tqdm(
+            executor.map(lambda x: process_mem(x, length), mem_paths),
+            total=len(mem_paths),
+            desc="Generating QAs from memories"
+        ))
+    
+    with open(f"data/sft/actgen/qas_from_mems/qas_length_{length}.jsonl", "w") as f:
+        for qa in qas_from_mems:
+            f.write(json.dumps(qa) + "\n")
+    # lengths = [5, 10, 15, 20, 25, 30]
+
+    # for length in lengths:
+    #     print(f"length: {length}")
+    #     route, route_contents, res = generate_qa_from_mem(args.mem_path, length)
+    #     print(res)
+    #     print(f"related nodes: {route}")
+    #     print("-"*20)
     
 if __name__ == "__main__":
     main()
