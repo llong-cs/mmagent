d54cd75aa49d65bcb1d0bdd2d267aef1bdad4bb3
llong-cs <kylin0long@gmail.com>
2025-05-19 18:22:49 +0800

    add check unmatched ids

--- analysis/check_invalid_memories.py
+++ analysis/check_invalid_memories.py
@@ -53,25 +53,47 @@ def check_invalid_memories(video_graph, mode):
         raise ValueError(f"Invalid mode: {mode}")
     # print(invalid_num, len(translated_mems))
     return invalid_num, num
-        
+
+def check_unmatched_ids(outputs_dir):
+    for file in tqdm(os.listdir(outputs_dir)):
+        if file.endswith(".json"):
+            with open(os.path.join(outputs_dir, file), "r") as f:
+                data = json.load(f)
+                input = data[0]
+                output = data[1]
+                input_str = ""
+                for c in input["content"]:
+                    if c["type"] == "text":
+                        input_str += c["text"]
+                        input_str += "\n"
+                output_str = output["content"][0]["text"]
+                pattern = r'<([^<>]*_[^<>]*)>'
+                entity_strs = re.findall(pattern, output_str)
+                for entity_str in entity_strs:
+                    if entity_str not in input_str:
+                        print(file)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mems_dir", type=str, default="/mnt/hdfs/foundation/longlin.kylin/mmagent/data/mems_qwen_0511/CZ_1")
+    parser.add_argument("--outputs_dir", type=str, default="/mnt/bn/videonasi18n/longlin.kylin/mmagent/data/sft/memgen/0511/evaluation/checkpoint-2214/val_gen")
     parser.add_argument("--mode", type=str, default="perclip")
     args = parser.parse_args()
     
-    mems_dir = args.mems_dir
-    mems_paths = [os.path.join(mems_dir, f) for f in os.listdir(mems_dir) if f.endswith(".pkl")]
-    total_invalid_num = 0
-    total_num = 0
-    for mems_path in tqdm(mems_paths):
-        video_graph = load_video_graph(mems_path)
-        invalid_num, num = check_invalid_memories(video_graph, args.mode)
-        total_invalid_num += invalid_num
-        total_num += num
-    print(total_invalid_num)
-    print(total_num)
-    print(f"Invalid memories ratio: {total_invalid_num / total_num}")
+    # mems_dir = args.mems_dir
+    # mems_paths = [os.path.join(mems_dir, f) for f in os.listdir(mems_dir) if f.endswith(".pkl")]
+    # total_invalid_num = 0
+    # total_num = 0
+    # for mems_path in tqdm(mems_paths):
+    #     video_graph = load_video_graph(mems_path)
+    #     invalid_num, num = check_invalid_memories(video_graph, args.mode)
+    #     total_invalid_num += invalid_num
+    #     total_num += num
+    # print(total_invalid_num)
+    # print(total_num)
+    # print(f"Invalid memories ratio: {total_invalid_num / total_num}")
+    
+    check_unmatched_ids(args.outputs_dir)
 
 if __name__ == "__main__":
     main()
