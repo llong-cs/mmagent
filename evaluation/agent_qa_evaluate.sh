cf3e53bb15212a200cf6b93afebc478e28a4db62
longlin.kylin <longlin.kylin@bytedance.com>
2025-05-21 12:36:08 +0000

    update sth

--- evaluation/agent_qa_evaluate.sh
+++ evaluation/agent_qa_evaluate.sh
@@ -4,7 +4,12 @@ sample_rounds=1
 output_dir="data/annotations/results"
 ours_version="0511"
 dataset="data/annotations/small_test.jsonl"
-model="qwen"
+
 # retrieval_model="gpt-4o-2024-11-20"
 retrieval_model="gemini-1.5-pro-002"
+
+model="qwen"
+python3 evaluation/qa.py --dataset ${dataset} --sample_rounds ${sample_rounds} --version ${ours_version} --model ${model} --retrieval_model ${retrieval_model} --output_dir ${output_dir} > logs/qa.out
+
+model="gemini"
 python3 evaluation/qa.py --dataset ${dataset} --sample_rounds ${sample_rounds} --version ${ours_version} --model ${model} --retrieval_model ${retrieval_model} --output_dir ${output_dir} > logs/qa.out
