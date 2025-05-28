cdfb52dc866c1bf07f6753fa198e6ef9a944bdae
llong-cs <kylin0long@gmail.com>
2025-05-28 11:52:25 +0800

    add prepare mlvu data

--- data_preparation/prepare_benchmark_data.py
+++ data_preparation/prepare_benchmark_data.py
@@ -39,16 +39,44 @@ def prepare_mlvu_data():
     data_dir = "/mnt/hdfs/foundation/longlin.kylin/mmagent/benchmarks/MLVU/MLVU/json"
     videos_dir = "/mnt/hdfs/foundation/longlin.kylin/mmagent/benchmarks/MLVU/MLVU/video"
     data_list = os.listdir(data_dir)
+    outputs = []
     for file in data_list:
         marker = file.split(".")[0]
         file_path = os.path.join(data_dir, file)
         with open(file_path, "r") as f:
             data = json.load(f)
             for sample in data:
+                video_id = sample["video"].split(".")[0]
                 video_path = os.path.join(videos_dir, marker, sample["video"])
-                if os.
-                
+                clip_path = os.path.join(clips_dir, "MLVU", marker, video_id)
+                mem_path = os.path.join(mems_dir, "MLVU", generate_file_name(video_path)+".pkl")
+                if os.path.exists(video_path):
+                    if sample["question_type"] not in ["subPlot", "summary"]:
+                        outputs.append({
+                            "video_id": video_id,
+                            "video_path": video_path,
+                            "clip_path": clip_path,
+                            "mem_path": mem_path,
+                            "question": sample["question"] + "\nOptions: " + " ".join(sample["candidates"]),
+                            "answer": sample["answer"],
+                        })
+                    else:
+                        outputs.append({
+                            "video_id": video_id,
+                            "video_path": video_path,
+                            "clip_path": clip_path,
+                            "mem_path": mem_path,
+                            "question": sample["question"],
+                            "answer": sample["answer"],
+                        })
+    print(len(outputs))
+    with open("data/benchmarks/MLVU.json", "w") as f:
+        json.dump(outputs, f, indent=4, ensure_ascii=False)
+    for output in outputs:
+        with open("data/benchmarks/MLVU.jsonl", "a") as f:
+            f.write(json.dumps(output, ensure_ascii=False)+"\n")
 
- 
+
 if __name__ == "__main__":
-    prepare_video_mme_data()
+    # prepare_video_mme_data()
+    prepare_mlvu_data()
